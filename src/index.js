b8e6eebaa091b69b5d51bc6f6fbb461c06d07149
Aleh <oleh@tut.by>
2022-01-05 12:54:44 +0300

    feat: fix error
    feat: fix error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function toReadable (number){
 let num='';
 let h=' hundred ';
-var arrN = String(number)  ;
+//var arrN = String(number)  ;
 let n={
     0: 'zero',    1: 'one',    2: 'two',    3: 'three',
     4: 'four',    5: 'five',    6: 'six',    7: 'seven',
@@ -18,18 +18,18 @@ num = n[number]
 } else if ((number % 10 ===0) && (number <100)){
   num = n[number]
 } else if (number <100){
-//let arrN = String(number)  
+let arrN = String(number)  
   num = n[arrN[0]*10] + ' '+n[arrN[1]]
 } else if ((number % 100 ===0) && (number <1000)){
 num = n[number / 100] + h
 } else if ((number % 10 === 0) && (number <1000)){
-//let arrN = String(number)   
+let arrN = String(number)   
 num = n[arrN[0]] + h + n[arrN[1]*10]
 } else if ((number % 100 < 20) && (number <1000)){
 let arrN = String(number)
 num = n[arrN[0]]+h+n[Number(arrN[1]*10)+Number(arrN[2])]
 } else if ((number % 100 > 20) && (number <1000)){
-//let arrN = String(number)
+let arrN = String(number)
 num = n[arrN[0]]+h+n[arrN[1]*10]+' '+n[arrN[2]]
 } else {num = number+' more than 1000'}
 return num
